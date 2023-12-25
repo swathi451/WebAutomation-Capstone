9808dc94ac5fa3ed3059d28f13a3bfdb9f62ecb8
TestVagrant <testvagrant@TestVagrants-MacBook-Pro-3.local>
2023-12-25 19:42:22 +0530

    add base class

--- src/main/java/pages/BaseClass.java
+++ src/main/java/pages/BaseClass.java
@@ -1,16 +1,32 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
-    protected PageWaits waits;
-    protected WebDriver webDriver;
-    protected PageActions actions;
 
-    public BaseClass(WebDriver webDriver) {
+//    protected PageWaits waits;
+   protected WebDriver webDriver;
+//    protected PageActions actions;
+//
+//    public BaseClass(WebDriver webDriver) {
+//
+//        this.webDriver = webDriver;
+//        this.waits = new PageWaits(webDriver);
+//        this.actions = new PageActions(webDriver);
+//    }
+@BeforeMethod
+public void setUp() {
+   // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+    WebDriver driver = new ChromeDriver();
+}
 
-        this.webDriver = webDriver;
-        this.waits = new PageWaits(webDriver);
-        this.actions = new PageActions(webDriver);
+    @AfterMethod
+    public void tearDown() {
+        if (webDriver!= null) {
+           webDriver.quit();
+        }
     }
 }
