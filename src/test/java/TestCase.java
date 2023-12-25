1de6e2fe7214cbbceb03a68952decdeda94a3af0
TestVagrant <testvagrant@TestVagrants-MacBook-Pro-3.local>
2023-12-25 19:32:38 +0530

    add testcase

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+import pages.BaseClass;
+
+public class TestCase extends BaseClass {
+    @Test
+    public void testMethod1() {
+        // Test logic using the WebDriver instance
+    }
+}
